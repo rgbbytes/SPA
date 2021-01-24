fac007c5ce914a06caaf2b3f6752daa2b1d3a893
rgbbytes <63249836+rgbbytes@users.noreply.github.com>
2021-01-24 14:48:02 -0600

    Create spa.py
    rough draft

---
+++ groupAssignment/spa.py
@@ -0,0 +1,4 @@
+#This is a drop-in replacement with the option for student groups. It still maximizes collective satisfaction.
+#The preferences.txt file now has an additional field that comes before preferences
+#To implement a group, simply write "group name","group quantity","1st pick","2nd pick",etc.
+#To implement a student, simply write "student name","1","1st pick","2nd pick",etc.
